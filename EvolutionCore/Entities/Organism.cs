12edb66aeaa1925f5a80743b9866344bb7eb92cf
Octavia <Octavia@DESKTOP-D4FDS0T>
2022-07-05 14:25:08 +0200

    Add project files.

---
+++ EvolutionCore/Entities/Organism.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvolutionCore.Entities
+{
+    public class Organism
+    {
+        public int Id { get; set; }
+        public string Sequance { get; set; }
+        public int Age { get; set; }
+        public int NChilderen { get; set; }
+        public string Ancestry { get; set; }
+    }
+}
