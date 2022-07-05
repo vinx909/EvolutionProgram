09b7e3d53de78781213fe8dc04a81b055b4b93dd
Octavia <vincent.theune@upcmail.nl>
2022-07-05 14:35:50 +0200

    completion of the most basic organism properties

--- EvolutionCore/Entities/Organism.cs
+++ EvolutionCore/Entities/Organism.cs
@@ -8,10 +8,29 @@ namespace EvolutionCore.Entities
 {
     public class Organism
     {
+        /// <summary>
+        /// the Id of the organism. purely relavent so the correct organism can be called for
+        /// </summary>
         public int Id { get; set; }
-        public string Sequance { get; set; }
+        /// <summary>
+        /// the sequance that defines the features of the organism
+        /// </summary>
+        public string Sequence { get; set; }
+        /// <summary>
+        /// the number of time intervals the organism has survived for
+        /// </summary>
         public int Age { get; set; }
+        /// <summary>
+        /// defines if the Organism is still alive
+        /// </summary>
+        public bool Alive { get; set; }
+        /// <summary>
+        /// defines how many childeren the organism has had
+        /// </summary>
         public int NChilderen { get; set; }
+        /// <summary>
+        /// keeps track of all parents of the organism
+        /// </summary>
         public string Ancestry { get; set; }
     }
 }
